a61bae149ca63fdeaae3f9d0f4552e964cbce37e
Joao Uchoa <joaodep2@gmail.com>
2021-07-02 11:21:47 -0300

    CRUD Scaffolding

---
+++ SalesWebPro/Data/SalesWebProContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebPro.Models;
+
+namespace SalesWebPro.Data
+{
+    public class SalesWebProContext : DbContext
+    {
+        public SalesWebProContext (DbContextOptions<SalesWebProContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebPro.Models.Departament> Departament { get; set; }
+    }
+}
